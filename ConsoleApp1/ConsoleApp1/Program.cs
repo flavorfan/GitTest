cd65bbb8cd4bb2efe575fea80c0afce9a0e61f4b
fan chuankang <flavorfan@tencent.com>
2018-07-28 18:42:23 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // code is edited in github
+            // code is edited in github  second time
         }
     }
 }
